01d624655ee3382c03d00e17505a07e1abc507dd
lelloman <cerasuolodomenico@gmail.com>
2019-01-25 22:24:32 +0100

    Converted gradle scripts to kts

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.20.0"
+}
+
+buildscript {
+    extra["assert_j_version"] = "3.11.1"
+    extra["constraint_layout_version"] = "1.1.3"
+    extra["dagger_version"] = "2.20"
+    extra["kotlin_version"] = "1.3.20"
+    extra["lifecycle_version"] = "1.1.1"
+    extra["mockito_version"] = "1.6.0"
+    extra["picasso_version"] = "2.71828"
+    extra["room_version"] = "1.1.1"
+    extra["rx_android_version"] = "2.1.0"
+    extra["rx_java_version"] = "2.2.4"
+    extra["support_lib_version"] = "28.0.0"
+
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+task("clean", Delete::class) {
+    delete = setOf(rootProject.buildDir)
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
+                    .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
+                    .any { it.matches(candidate.version) }
+                if (rejected) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
+    // optional parameters
+    checkForGradleUpdate = true
+    outputFormatter = "json"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
+}
---
+++ library/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("kotlin-kapt")
+    id("com.github.dcendents.android-maven")
+}
+
+group = "com.github.lelloman"
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    dataBinding {
+        isEnabled = true
+    }
+
+    buildTypes {
+        findByName("release")?.apply {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+
+    val supportLibVersion = rootProject.extra["support_lib_version"]
+    val kotlinVersion = rootProject.extra["kotlin_version"]
+    val daggerVersion = rootProject.extra["dagger_version"]
+    val lifecycleVersion = rootProject.extra["lifecycle_version"]
+    val roomVersion = rootProject.extra["room_version"]
+    val mockitoVersion = rootProject.extra["mockito_version"]
+    val assertJVersion = rootProject.extra["assert_j_version"]
+
+    // support
+    api("com.android.support:appcompat-v7:$supportLibVersion")
+    api("com.android.support:recyclerview-v7:$supportLibVersion")
+    api("com.android.support:design:$supportLibVersion")
+    api("com.android.support:cardview-v7:$supportLibVersion")
+    api("com.android.support.constraint:constraint-layout:${rootProject.extra["constraint_layout_version"]}")
+    api("android.arch.lifecycle:extensions:$lifecycleVersion")
+
+    // kotlin
+    api("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+    api("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    api("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+
+    // rx
+    api("io.reactivex.rxjava2:rxandroid:${rootProject.extra["rx_android_version"]}")
+    api("io.reactivex.rxjava2:rxjava:${rootProject.extra["rx_java_version"]}")
+
+    // dagger
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    implementation("com.google.dagger:dagger-android:$daggerVersion")
+    kapt("com.google.dagger:dagger-android-processor:$daggerVersion")
+    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
+
+    implementation("com.squareup.picasso:picasso:${rootProject.extra["picasso_version"]}")
+
+    testImplementation("junit:junit:4.12")
+    testImplementation("android.arch.persistence.room:testing:$roomVersion")
+    testImplementation("com.nhaarman:mockito-kotlin:$mockitoVersion")
+    testImplementation("org.assertj:assertj-core:$assertJVersion")
+    testImplementation("android.arch.core:core-testing:$lifecycleVersion")
+
+    androidTestImplementation("com.github.lelloman:android-instrumented-tests-utils:-SNAPSHOT")
+}
