421c7913ca209cd241eaa76f68cea250b4018e69
lelloman <cerasuolodomenico@gmail.com>
2019-02-24 18:15:09 +0100

    Add some utility methods to Screen

--- androidtestutils/src/main/java/com/lelloman/common/androidtestutils/Screen.kt
+++ androidtestutils/src/main/java/com/lelloman/common/androidtestutils/Screen.kt
@@ -1,19 +1,38 @@
 @file:Suppress("unused")
 
-package com.lelloman.instrumentedtestutils
+package com.lelloman.common.androidtestutils
 
 import android.content.Context
 import androidx.annotation.IdRes
 import androidx.annotation.StringRes
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import com.lelloman.instrumentedtestutils.ViewActions
+import com.lelloman.instrumentedtestutils.ViewAssertions
 
 abstract class Screen {
 
-    private val context: Context = ApplicationProvider.getApplicationContext()
+    val context: Context = ApplicationProvider.getApplicationContext()
 
-    protected fun viewVisible(@IdRes id: Int) = ViewAssertions.checkViewIsDisplayed(id)
-    protected fun viewVisible(text: String) = ViewAssertions.checkViewWithTextIsDisplayed(text)
+    fun viewVisible(@IdRes id: Int) = ViewAssertions.checkViewIsDisplayed(id)
+    fun viewVisible(text: String) = ViewAssertions.checkViewWithTextIsDisplayed(text)
 
-    protected fun string(@StringRes id: Int): String = context.getString(id)
-    protected fun string(@StringRes id: Int, vararg arg: Any): String = context.getString(id, *arg)
+    fun string(@StringRes id: Int): String = context.getString(id)
+    fun string(@StringRes id: Int, vararg arg: Any): String = context.getString(id, *arg)
+
+    fun <T : Screen> showsScreen(screenCreator: () -> T): T = screenCreator()
 }
+
+fun <T : Screen> T.wait(seconds: Double) = apply { com.lelloman.instrumentedtestutils.wait(seconds) }
+
+fun <T : Screen> T.clickOnViewWithText(@StringRes stringResId: Int) = apply {
+    ViewActions.clickViewWithText(string(stringResId))
+}
+
+fun <T : Screen> T.rotateLeft() = apply { com.lelloman.instrumentedtestutils.rotateLeft() }
+fun <T : Screen> T.rotateRight() = apply { com.lelloman.instrumentedtestutils.rotateRight() }
+fun <T : Screen> T.rotateNatural() = apply { com.lelloman.instrumentedtestutils.rotateNatural() }
+
+fun <T : Screen> T.closeKeyboard() = apply { Espresso.closeSoftKeyboard() }
+
+fun <T : Screen> T.hasText(text: String) = apply { ViewAssertions.checkViewWithTextIsDisplayed(text) }
