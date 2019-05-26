fd047e35164b46708b113e0d53259b3385fd8559
lelloman <cerasuolodomenico@gmail.com>
2019-05-26 10:13:25 +0200

    Load WebView url from ViewModel

--- demoapp/src/main/java/com/lelloman/demoapp/ui/webview/WebViewViewModel.kt
+++ demoapp/src/main/java/com/lelloman/demoapp/ui/webview/WebViewViewModel.kt
@@ -10,6 +10,13 @@ class WebViewViewModel(dependencies: Dependencies) : BaseViewModel(dependencies)
     private val mutableProgressVisible = MutableLiveData<Boolean>()
     val progressVisible: LiveData<Boolean> = mutableProgressVisible
 
+    private val mutableUrl = MutableLiveData<String>().apply {
+        value = "https://www.imslp.org"
+//        value = "https://www.orimi.com/pdf-test.pdf"
+//        value = "https://imslp.org/wiki/Special:IMSLPDisclaimerAccept/548820"
+    }
+    val url: LiveData<String> = mutableUrl
+
     override fun onPageLoadingStateChanged(percent: Int) {
         mutableProgressVisible.postValue(percent != 100)
     }
