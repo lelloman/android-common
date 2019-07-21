0145013561c707d0cbb9bbb4e0fadcc593549100
lelloman <cerasuolodomenico@gmail.com>
2019-07-21 12:09:34 +0200

    Add OnKeyboardActionDoneListener BindingAdapter

--- core/src/main/java/com/lelloman/common/utils/BindingAdapters.kt
+++ core/src/main/java/com/lelloman/common/utils/BindingAdapters.kt
@@ -3,7 +3,13 @@
 package com.lelloman.common.utils
 
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+
+interface OnKeyboardActionDoneListener {
+    fun onKeyboardActionDone()
+}
 
 object BindingAdapters {
 
@@ -11,5 +17,18 @@ object BindingAdapters {
     @BindingAdapter("viewVisible")
     fun bindViewVisibility(view: View, isVisible: Boolean) {
         view.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+    @JvmStatic
+    @BindingAdapter("onKeyboardActionDoneListener")
+    fun setOnKeyboardActionDoneListener(view: TextView, listener: OnKeyboardActionDoneListener?) {
+        if (listener == null) {
+            view.setOnEditorActionListener(null)
+        } else {
+            view.setOnEditorActionListener { _, actionId, _ ->
+                (actionId == EditorInfo.IME_ACTION_DONE)
+                    .also { if (it) listener.onKeyboardActionDone() }
+            }
+        }
     }
 }
