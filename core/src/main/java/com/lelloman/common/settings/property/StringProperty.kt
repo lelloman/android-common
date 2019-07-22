c278e0930d5173a268d96e910de18a7581e64c4e
lelloman <cerasuolodomenico@gmail.com>
2019-07-22 09:29:13 +0200

    Add prefs StringProperty

---
+++ core/src/main/java/com/lelloman/common/settings/property/StringProperty.kt
@@ -0,0 +1,16 @@
+package com.lelloman.common.settings.property
+
+import android.content.SharedPreferences
+
+class StringProperty(
+    key: String,
+    default: String,
+    prefs: SharedPreferences
+) : PrefsProperty<String>(key, default, prefs) {
+
+    override fun putValueInPrefs(editor: SharedPreferences.Editor, value: String) {
+        editor.putString(key, value)
+    }
+
+    override fun getValueFromPrefs(prefs: SharedPreferences) = prefs.getString(key, default)!!
+}
