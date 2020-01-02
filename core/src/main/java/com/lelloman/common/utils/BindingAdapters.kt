e1c3a3838ce473dfc03c6e6aea752e3814497964
lelloman <cerasuolodomenico@gmail.com>
2020-01-02 18:49:41 +0100

    Add visible and visibleGone binding adapters

--- core/src/main/java/com/lelloman/common/utils/BindingAdapters.kt
+++ core/src/main/java/com/lelloman/common/utils/BindingAdapters.kt
@@ -13,10 +13,24 @@ interface OnKeyboardActionDoneListener {
 
 object BindingAdapters {
 
+    @Suppress("DeprecatedCallableAddReplaceWith")
+    @Deprecated(message = "use \"visible\" or \"visibleGone\"")
     @JvmStatic
     @BindingAdapter("viewVisible")
     fun bindViewVisibility(view: View, isVisible: Boolean) {
         view.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+    @JvmStatic
+    @BindingAdapter("visible")
+    fun bindVisible(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.INVISIBLE
+    }
+
+    @JvmStatic
+    @BindingAdapter("visibleGone")
+    fun bindVisibleGone(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.GONE
     }
 
     @JvmStatic
