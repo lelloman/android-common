a966f747cd036053091d096e4f2e42267455a380
lelloman <cerasuolodomenico@gmail.com>
2020-04-30 10:08:36 +0200

    Add SharedPreferences.stringProperty extension

--- core/src/main/java/com/lelloman/common/data/settings/property/StringProperty.kt
+++ core/src/main/java/com/lelloman/common/data/settings/property/StringProperty.kt
@@ -2,6 +2,9 @@ package com.lelloman.common.data.settings.property
 
 import android.content.SharedPreferences
 
+fun SharedPreferences.stringProperty(key: String, default: String) =
+    StringProperty(key, default, this)
+
 class StringProperty(
     key: String,
     default: String,
